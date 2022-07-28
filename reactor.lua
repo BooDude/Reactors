6646373ca5f2a7b0a72fef70d8df25c48388c7b6
BooDude <boodude323@gmail.com>
2022-07-28 02:11:55 -0500

    Create reactor.lua

---
+++ reactor.lua
@@ -0,0 +1,5 @@
+-- Jah's Bigger Reactor/Turbine Program
+print("Finding reactor...")
+if(peripherals.find("BiggerReactors_Reactors_1")) then print("Reactor found!")
+print("Finding turbine...")
+if(peripherals.find("BiggerReactors_Turbines_1")) then print("Turbine found!")
